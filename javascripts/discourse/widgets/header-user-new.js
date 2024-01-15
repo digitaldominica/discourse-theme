d6903fb82560d676944d9f3790b1821302b4b01e
Digital Dominica <136180132+digitaldominica@users.noreply.github.com>
2024-01-15 06:39:39 -0400

    1

---
+++ javascripts/discourse/widgets/header-user-new.js
@@ -0,0 +1,11 @@
+import { hbs } from "ember-cli-htmlbars";
+import RenderGlimmer from "discourse/widgets/render-glimmer";
+import { createWidget } from "discourse/widgets/widget";
+
+export default createWidget("header-user-new", {
+  tagName: "li.header-dropdown-toggle.header-user-new",
+
+  html() {
+    return [new RenderGlimmer(this, "div", hbs`<HeaderUserNew />`)];
+  },
+});
